df6f96d07489e296b6847a2e21375ca302c984c8
Gokhan-Kayhan <74496005+Gokhan-Kayhan@users.noreply.github.com>
2022-01-18 05:55:05 +0100

    Create setup_folders.py

---
+++ Colab_Documents/setup_folders.py
@@ -0,0 +1,26 @@
+import os
+
+
+os.mkdir("/content/gdrive/My Drive/TensorFlow/scripts/")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/scripts/preprocessing")
+
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/images")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/images/train")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/images/test")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/annotations")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/pre-trained-models")
+
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/models")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/workspace/training_demo/models/my_ssd_resnet50_v1_fpn")
+
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme/after_divide")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme/detection_half")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme/detected_full")
+
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme2/circle_detected_full")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme2/circle_detected_half")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme2/csv_corner_point")
+os.mkdir("/content/gdrive/My Drive/TensorFlow/deneme2/final_detected_counted")
